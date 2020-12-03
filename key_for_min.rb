0f87b47e8b7f8be735a56ebfa60c3e377186c7cb
Matthew Kindt <matthewkindt96@yahoo.com>
2020-12-03 21:58:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   min_key = :key
-  name_hash.each do |name_hash, key|
+  name_hash.each do |name_hash|
     if name_hash == :key.min
       min_key = key
     end
